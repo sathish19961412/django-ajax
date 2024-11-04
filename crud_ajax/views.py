15e32b14b87160e287526ed447c5182dafb864c8
sathish19961412 <sathishfreelanc5@gmail.com>
2024-11-04 13:29:56 +0530

    create crudproject and ajax_crud

---
+++ crud_ajax/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import CrudUser
+
+class CrudView(ListView):
+   model = CrudUser
+   template_name ="crud_ajax/crud.html"
+   context_object_name='users'
