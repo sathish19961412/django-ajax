10fc5ed77075f9d551a3c4d9c41c85a2bb3dd206
sathish19961412 <sathishfreelanc5@gmail.com>
2024-11-04 17:53:32 +0530

    Compeleted to the django and ajax

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,5 +19,8 @@ from django.urls import path
 from crud_ajax import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crud/',views.CrudView.as_view(),name='crud_ajax')
+    path('crud/',views.CrudView.as_view(),name='crud_ajax'),
+    path('ajax/crud/create/', views.CreateCrudUser.as_view(), name='crud_ajax_create'),
+    path('ajax/crud/update/', views.UpdateCrudUser.as_view(), name='crud_ajax_update'),
+     path('ajax/crud/delete/', views.DeleteCrudUser.as_view(), name='crud_ajax_delete'),
 ]
